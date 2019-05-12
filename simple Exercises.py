393bbb6aa51b5f4c397129764c860752b650550e
Josney1223 <49200169+Josney1223@users.noreply.github.com>
2019-05-12 19:17:36 -0300

    Update

--- simple Exercises.py
+++ simple Exercises.py
@@ -4,8 +4,192 @@ import random
 def Wait():
     k = input("Press enter to continue")
 
-# Exercício filas exército
-def soldiers():
+## Exercicios Fáceis
+# Fatorial
+def Fatorial(): 
+    n = int(input("Digite um número: "))
+    fat = 1
+    while ( n > 0):
+        fat *= n
+        n -= 1
+        
+    print(fat)
+                
+# Triângulo
+def Triangulo():
+    ladoA = float(input("Digite o valor do primeiro lado: "))
+    ladoB = float(input("Digite o valor do segundo lado: "))
+    ladoC = float(input("Digite o valor do terceiro lado: "))
+
+    class Triangulo:
+        def __init__(self, A, B, C):
+            self.A = A
+            self.B = B
+            self.C = C
+            self.y = 0
+            
+        def verificarSoma(self):
+            if (self.A > self.B + self.C):
+                x = ("False")
+                self.y = 1
+            elif (self.B > self.A + self.C):
+                x = ("False")
+                self.y = 1
+            elif (self.C > self.A + self.B):
+                x = ("False")
+                self.y = 1
+            else:
+                x = ("True")
+            print("É um triângulo: " + x)
+                
+        def equilatero(self):
+            if (self.A == self.B and self.B == self.C):
+                print("É equilátero? True")
+            else:
+                print("É equilátero? False")
+                
+        def isoceles(self):
+            if (self.A == self.B or self.B == self.C):
+                print("É isóceles? True")
+            else:
+                print("É isóceles? False")
+                
+        def escaleno(self):
+            if (self.A != self.B and self.B != self.C):
+                print("É escaleno? True")
+            else:
+                print("É escaleno? False")
+
+    tri = Triangulo(ladoA,ladoB,ladoC)
+    tri.verificarSoma()
+    if (tri.y != 1):
+        tri.equilatero()
+        tri.isoceles()
+        tri.escaleno()
+
+# Exercício eleição
+def Election():
+    expectedPeopleNumber = int(input("Digite o número de eleitoes esperados: "))
+    colectedPeopleNumber = int(input("Digite o número de votos coletados: "))
+    peopleVoteWhite = int(input("Digite o número de votos em branco: "))
+    peopleVoteNull = int(input("Digite o número de votos em nulo: "))
+
+    print("Dos", expectedPeopleNumber, "eleitores esperados,", colectedPeopleNumber,
+          "eleitores compareceram, isso sendo", colectedPeopleNumber/expectedPeopleNumber*100, "% dos eleitores esperados.")
+    print("Desses eleitores,", peopleVoteWhite/colectedPeopleNumber*100,
+          "% votaram em branco e", peopleVoteNull/colectedPeopleNumber*100, "% votaram nulo.")
+
+# Exercício média da nota
+def Media():
+    P1 = int(input('Digite a nota da primeira prova: '))
+    A1 = int(input('Digite a nota da primeira atividade: '))
+    P2 = int(input('Digite a nota da segunda prova: '))
+    A2 = int(input('Digite a nota da segunda atividade: '))
+    N1 = (P1 + A1*3) / 4
+    N2 = (P2 + A2*3) / 4
+    NF = (N1 + N2) / 2
+    print("A média final dessa pessoa é:", NF)
+
+# Mínimo multiplo comum
+def Mdc():
+    valor = int(input("Digite um número: "))
+    divisor = int(input("Digite o valor do divisor: "))
+    resto = 1
+    while (resto != 0):
+        resto = valor % divisor
+        if (resto == 0):
+            print("MDC = " + str(divisor))
+        else:
+            valor = divisor
+            divisor = resto
+
+# Operações com números primos
+def Prime():
+    def Primo(x):
+        y = 1
+        divisores = 0
+        while(y <= x):
+            if (x % y == 0):
+                divisores += 1
+                y += 1
+            else:
+                y += 1
+        if (divisores == 2):
+            return True
+        else:
+            return False
+
+    def Divisivel(x,y):
+        if (x % y == 0):
+            return True
+        else:
+            return False
+        
+    def Primos(n):
+        x = 1
+        while (x <= n):        
+            if Primo(x):
+                print(x, end = ', ')
+            x += 1
+                    
+    def info():
+        print(" ")
+        print("------------------/------------------")
+        print("0. Exit")
+        print("1. Número Primo")
+        print("2. Divisibilidade de X por Y")
+        print("3. Todos os números primos até N", end = '\n\n')
+        
+    while True:
+        info()
+        operacao = int(input("Digite a operação que deseja realizar: "))
+        if (operacao == 0):
+            exit()
+        elif (operacao == 1):
+            x = int(input("Digite o número: "))
+            if Primo(x):
+                print("É um número primo")
+            else:
+                print("Não é número primo")
+        elif (operacao == 2):
+            x = int(input("Digite o valor de X: "))
+            y = int(input("Digite o valor de Y: "))
+            if Divisivel(x,y):
+                print(str(x) + " é divisível por " + str(y))
+            else:
+                print(str(x) + " não é divisível por " + str(y))
+        elif (operacao == 3):
+            n = int(input("Digite o valor de n: "))
+            Primos(n)
+
+## Exercicios Dificeis
+# Leilão
+def Auction():
+    lances = []
+    person = []
+    maxNumber = 0
+        
+    while True:
+        x = int(input("Digite o seu lance: "))
+        lances.append(x)
+        z = input("Quem fez esse lance? ")
+        person.append(z)
+        y = input("Acabaram os lances? <sim/nao> ").lower().strip()
+        if y == 'sim':
+            break
+        else:
+            continue
+
+    while True:
+        for x in range (len(lances)):
+            if maxNumber < int(lances[x]):
+                maxNumber = int(lances[x])
+                personMaxNumber = person[x]
+        print("O maior lance é:", maxNumber, "de", personMaxNumber)
+        break
+
+# Filas exército
+def Soldiers():
     print("Esse programa foi desenvolvido com o intuito",
           " de demonstrar um posicionamento de um batalhao.", sep='\n')
     x = int(input("Digite um número de soldados: "))
@@ -22,31 +206,26 @@ def soldiers():
     if x < 0:
         print("e restaram", x + n, "soldados na última fileira.")
 
-# Exercício eleição
-def election():
-    expectedPeopleNumber = int(input("Digite o número de eleitoes esperados: "))
-    colectedPeopleNumber = int(input("Digite o número de votos coletados: "))
-    peopleVoteWhite = int(input("Digite o número de votos em branco: "))
-    peopleVoteNull = int(input("Digite o número de votos em nulo: "))
-
-    print("Dos", expectedPeopleNumber, "eleitores esperados,", colectedPeopleNumber,
-          "eleitores compareceram, isso sendo", colectedPeopleNumber/expectedPeopleNumber*100, "% dos eleitores esperados.")
-    print("Desses eleitores,", peopleVoteWhite/colectedPeopleNumber*100,
-          "% votaram em branco e", peopleVoteNull/colectedPeopleNumber*100, "% votaram nulo.")
-
-# Exercício média da nota
-def media():
-    P1 = int(input('Digite a nota da primeira prova: '))
-    A1 = int(input('Digite a nota da primeira atividade: '))
-    P2 = int(input('Digite a nota da segunda prova: '))
-    A2 = int(input('Digite a nota da segunda atividade: '))
-    N1 = (P1 + A1*3) / 4
-    N2 = (P2 + A2*3) / 4
-    NF = (N1 + N2) / 2
-    print("A média final dessa pessoa é:", NF)
+# Fibonacci
+def Fibonacci():
+    n = int(input("Digite até que número deseja chegar na sequência Fibonacci: "))
+    for x in range(0, n):
+        if x == 0:
+            y = 0
+            print("fib(" + str(x)+") =", y)
+            y2 = y
+        elif x == 1:
+            y += 1
+            print("fib(" + str(x)+") =", y)
+            y1 = y
+        else:
+            print("fib(" + str(x)+") =", y)
+            y2 = y1
+            y1 = y
+            y = y1 + y2
 
 # Conversor de data
-def data():
+def Data():
 
     def StringToArray(word):
         wordArray = []
@@ -116,7 +295,7 @@ def data():
         break
 
 # Jogo Jokempo
-def game():
+def Jokempo():
     Cpontuacao = 0
     Jpontuacao = 0
     matches = 0
@@ -175,21 +354,51 @@ def game():
 def Hub():
     while True:
         os.system('cls')
-        print("1 - Soldados", "2 - Eleicoes", "3 - Média Notas",
-              "4 - Conversao de data", "5 - Jokempo", "", "0 - Exit", sep="\n")
-        y = input("Digite sua escolha: ").strip()
-        if y == "0":
+        print("1 - Exercicios Dificeis", "2 - Exercicios Faceis", "", "0 - Exit", sep="\n")
+        x = input("Digite sua escolha: ").strip()
+
+        if x == '0':
             exit()
-        elif y == '1':
-            soldiers()
-        elif y == '2':
-            election()
-        elif y == '3':
-            media()
-        elif y == '4':
-            data()
-        elif y == '5':
-            game()
-        Wait()
+
+        elif x == '1':
+            while True:
+                os.system('cls')
+                print("1 - Leilão", "2 - Soldados", "3 - Fibonacci", "4 - Conversao de data", "5 - Jokempo", "", "0 - Exit", sep="\n")
+                y = input("Digite sua escolha: ").strip()
+                if y == "0":
+                    exit()
+                elif y == '1':
+                    Auction()
+                elif y == '2':
+                    Soldiers()
+                elif y == '3':
+                    Fibonacci()
+                elif y == '4':
+                    Data()
+                elif y == '5':
+                    Jokempo()
+
+                Wait()
+
+        elif x == '2':
+            while True:
+                os.system('cls')
+                print("1 - Fatorial", "2 - Triângulo", "3 - Eleição", "4 - Média Final", "5 - Mínimo Multiplo Comum", "6 - Números Primos", "0 - Exit", sep="\n")
+                y = input("Digite sua escolha: ").strip()
+                if y == "0":
+                    exit()
+                elif y == '1':
+                    Fatorial()
+                elif y == '2':
+                    Triangulo()
+                elif y == '3':
+                    Election()
+                elif y == '4':
+                    Media()
+                elif y == '5':
+                    Mdc()
+                elif y == '6':
+                    Prime()
+                Wait()
 
 Hub()
