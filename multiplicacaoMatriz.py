79fc386aab81dde12b3dc71278e097693ddbfeaf
Josney1223 <49200169+Josney1223@users.noreply.github.com>
2019-05-28 09:41:39 -0300

    Update

--- multiplicacaoMatriz.py
+++ multiplicacaoMatriz.py
@@ -1,20 +1,21 @@
-matrizMxN = [[1,2],
-             [1,2]]
+matrizMxN = [[1,2,3],
+             [1,2,3]]
 
 matrizAxB = [[1,2],
+             [1,2],
              [1,2]]
 
 # Calculo matrizF
 matrizF = []
-for high in range(len(matrizAxB)):
+for high in range(len(matrizMxN)):
     matrizF.append([])
-    for lenght in range(len(matrizMxN[0])):
+    for lenght in range(len(matrizAxB[0])):
         matrizF[high].append(0)
  
 for m in range(len(matrizF)):
     for n in range(len(matrizF[0])):
-        for i in range(len(matrizAxB)):
-            matrizF[m][n] += matrizAxB[m][i] * matrizMxN[i][n]
+        for i in range(len(matrizMxN[0])):
+            matrizF[m][n] += matrizAxB[i][n] * matrizMxN[m][i]
         
 for i in matrizF:
     print(i)
